c16a3aec282086465392926a7a973ecfe222e195
UtpalTheDev <utpalpati8@gmail.com>
2021-01-21 20:48:12 +0530

    refactor: first black scatching done

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-cont=document.querySelector("#container");
-window.addEventListener("load",grid);
-function grid(){
-    for(var i=0;i<16;i++){
+var cont=document.querySelector("#container");
+
+window.addEventListener("load",()=>{grid(16)});
+function grid(size){
+    var maxdiv=size*size;
+    cont.style.gridTemplateColumns=`repeat(${size},1fr)`;
+    cont.style.gridTemplateRows=`repeat(${size},1fr)`;
+    
+    for(var i=0;i<maxdiv;i++){
     var div=document.createElement("div");
+
     cont.appendChild(div);  
+
     }
+    var eachdiv=cont.querySelectorAll('div');
+    eachdiv.forEach((item)=>{
+        item.addEventListener('mouseover',colorset);
+        item.style.transition=`all 0.3s`;
+        console.log(item);
+    });
+}
+
+function colorset(){
+    this.style.backgroundColor=`black`;
+    this.style.transitionDelay=`0.1s`;
 }
