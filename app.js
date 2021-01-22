94629be9611f1928c669ea548930e6a08330797e
UtpalTheDev <utpalpati8@gmail.com>
2021-01-22 16:39:16 +0530

    feat: rainbow color added

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var range=document.querySelector("#range");
 //var colorbtn=document.querySelector("#color");
 var colorinput=document.querySelector("#colorinput");
 var eraser=document.querySelector("#eraser");
-
+var rainbow=document.querySelector("#rainbow");
+var rainbowsetvar=0;
 var color=`black`;
-
+rainbow.addEventListener('click',rainbowsetter);
 eraser.addEventListener('click',eraseperdiv);
 colorinput.addEventListener('input', colorset);
 //colorbtn.addEventListener('click', colorset);
@@ -35,6 +36,9 @@ function grid(size){
 }
 
 function colorpick(){
+    if(rainbowsetvar===1){
+        rainbowset();
+    }
     this.style.backgroundColor=`${color}`;
     this.style.transitionDelay=`0.1s`;
 }
@@ -47,18 +51,28 @@ function cleandiv(){
 }
 function sizechange(){
     cleandiv();
-    color=black;
+    color=`black`;
     grid(range.value);
 }
 function erase(){
+    rainbowsetvar=0;
     var eachdiv=cont.querySelectorAll('div');
     eachdiv.forEach((item)=>{
     item.style.backgroundColor=`antiquewhite`
     });
+    color=`black`;
 }
 function colorset(event){
+    rainbowsetvar=0;
     color=event.target.value;
 }
 function eraseperdiv(){
+    rainbowsetvar=0;
     color=`antiquewhite`;
+}
+function rainbowset(){
+    color=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+}
+function rainbowsetter(){
+    rainbowsetvar=1;
 }
